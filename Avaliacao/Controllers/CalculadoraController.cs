82210b9f06e93e2472d448c8126f5f4bc13c00cb
Denize Bassi Figueiredo <denize.f.bassi@gmail.com>
2022-12-03 16:47:12 -0300

    Add project files.

---
+++ Avaliacao/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using Avaliacao.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Avaliacao.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Somar(int num1=0, int num2=0)
+        {
+           int resultado = Calculadora.Calcula().Somar(num1, num2);
+            return View(resultado);
+        }
+        public ActionResult Subtrair(int num1 = 0, int num2 = 0)
+        {
+            int resultado = Calculadora.Calcula().Subtrair(num1, num2);
+
+            return View(resultado);
+        }
+        public ActionResult Multiplicar(int num1 = 0, int num2 = 0)
+        {
+            int resultado = Calculadora.Calcula().Multiplicar(num1, num2);
+
+            return View(resultado);
+        }
+        public ActionResult Dividir(int num1 = 0, int num2 = 0)
+        {
+            int resultado = Calculadora.Calcula().Dividir(num1, num2);
+
+            return View(resultado);
+        }
+    }
+}
---
+++ Avaliacao/Services/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avaliacao.Services
+{
+    public class Calculadora : ICalculadora
+    {
+        private Calculadora() { }
+
+        private static readonly Calculadora _instance = new Calculadora();
+
+        public static Calculadora Calcula()
+        {  
+            return _instance; 
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            if(num2 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1* num2;
+        }
+
+        public int Somar(int num1, int num2)
+        {
+           return num1 + num2;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+           return num1 - num2;
+        }
+    }
+}
